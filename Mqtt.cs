a60ff7590bfee372ed6c6f9b257829f6351767d9
Andreas Rupprecht <ruppreca@t-online.de>
2024-02-21 15:26:16 +0100

    Projektdateien hinzufügen.

---
+++ Mqtt.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+using System;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace Z_PumpControl_Raspi
+{
+    internal class Mqtt
+    {
+        private MqttClient _client;
+        private const string user = "raspi-ar";
+        private const string passwd = "LoTTPlaolGiM";
+        private const string mqttIp = "192.168.178.26";
+
+        private static Logger Log = LogManager.GetCurrentClassLogger();
+
+        public Mqtt()
+        {
+            _client = new MqttClient(mqttIp);
+            var r = _client.Connect(Guid.NewGuid().ToString(), user, passwd);
+            Log.Info($"Connect returns {r}");
+        }
+
+        public void publishZ(string value)
+        {
+            var r = _client?.Publish("HomeTemp/Z-PumpOn", Encoding.UTF8.GetBytes(value), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            //Log.Info($"Publish Z returns {r}");
+        }
+
+        public void publishWw(string value)
+        {
+            var r = _client?.Publish("HomeTemp/WWtemp", Encoding.UTF8.GetBytes(value), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            //Log.Info($"Publish Ww returns {r}");
+        }
+    }
+}
---
+++ ReadTemp.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using System;
+using System.IO;
+
+
+namespace Z_PumpControl_Raspi;
+
+internal static class Temp
+{
+    private const string OneWireIdT3 = @"/sys/bus/w1/devices/28-000000a85f2b/temperature";
+    private static Logger Log = LogManager.GetCurrentClassLogger();
+
+    public static int ReadWWtemp()
+    {
+        try
+        {
+            var data = File.ReadAllText(OneWireIdT3);
+            if (int.TryParse(data, out var value))
+            {
+                return value;
+            }
+            Log.Error($"Failed to parse to int: {data}");
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Info($"Failed to open file {OneWireIdT3}");
+            Log.Error($"{ex.Message}");
+        }
+        return 0;
+    }
+}
